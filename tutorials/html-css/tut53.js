bfafc913648611c4b4dde1d9b64618b926a9f2b6
SHALINI <drshaverma242@gmail.com>
2022-08-13 21:25:48 +0530

    tut53

---
+++ tutorials/html-css/tut53.js
@@ -0,0 +1,23 @@
+console.log("this is tutorial 53");
+function greet(name, greettext){
+    console.log(greettext + ' '+ name)
+    console.log(name + ' is a good girl')
+}
+
+function sum(a,b,c){
+    let d = a + b + c;
+    return d;
+}
+let name = 'honey';
+let name1 = 'shalini';
+let name2 = 'litilika';
+let name3 = 'Sania';
+let greettext = 'good afternoon'
+greet(name, greettext);
+greet(name1, greettext);
+greet(name2, greettext);
+greet(name3, greettext);
+let returnval = sum(1,2,3);
+// let returnval = greet(name3);
+console.log(returnval);
+// console.log(returnval);
