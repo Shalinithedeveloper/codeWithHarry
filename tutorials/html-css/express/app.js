1ee4f96cb6f3c36f7b63a8a3b511fb0feb3b4814
SHALINI <drshaverma242@gmail.com>
2022-09-22 00:14:28 +0530

    tut72

--- tutorials/html-css/express/app.js
+++ tutorials/html-css/express/app.js
@@ -1,7 +1,22 @@
-const express = require('express')
+const { application } = require('express');
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 80;
+
+app.use('/static', express.static('static'))
+
+//set the templates engine as pug
+app.set('view engine', 'pug')
+
+//set views directory
+app.set('views', path.join(__dirname, 'views'))
+
+//our pug demo endpoint
+app.get("/demo", (req, res)=>{
+res.status(200).render('demo', { title: 'Hey Shalini', message: 'Hello there! and thanks for telling me how to use postman' })
+})
 
 app.get("/", (req, res)=>{
     res.send("this is home page of my first express app with harry")
---
+++ tutorials/html-css/express/static/index.js
@@ -0,0 +1,1 @@
+console.log("Shalini is a good girl")
