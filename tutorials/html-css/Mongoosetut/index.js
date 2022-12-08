9c9a523a352f6a9652909c89327ef7e27aa0f38a
SHALINI <drshaverma242@gmail.com>
2022-12-08 23:52:53 +0530

    tut86

--- tutorials/html-css/Mongoosetut/index.js
+++ tutorials/html-css/Mongoosetut/index.js
@@ -1,10 +1,19 @@
 // getting-started.js
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ShaliniKart', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/ShaliniKart', { useNewUrlParser: true, useUnifiedTopology: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function(){
+db.once('open', function () {
     //we're connected!
     console.log('we are connected bro')
 });
+
+var kittySchema = new mongoose.Schema({
+    name: String
+});
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var shaliniKitty = new Kitten({ name: "shaliniKitty Name"});
+console.log(shaliniKitty.name);
